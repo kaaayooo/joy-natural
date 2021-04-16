1845f06d706033d61b8e2a3f59f65fb89a578788
藤田華代 <kayo@fujitahanayonoMacBook-Air.local>
2021-04-16 18:40:23 +0900

    joy_natural

---
+++ spring.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>spring</title>
+</head>
+<body>
+
+<div class="container-fluid">
+  <div class="card">
+    <div class="card-header">
+<!-- pic -->    
+    </div>
+
+    <div class="card-body">
+      <h4 class="text-center">Spring</h4>
+        <form action="" method="post" class="text-center">
+          <input type="checkbox" name="spring" id="spring" value="Cabbage"> 
+          <span class="text-center">Cabbage</span>
+          <input type="number" name="s_no" id="s_no">
+          <br>
+          <input type="checkbox" name="spring" id="spring" value="Asparagus">Asparagus
+          <br>
+          <input type="checkbox" name="spring" id="spring" value="Fava bean">Fava bean
+          <br>
+          <input type="checkbox" name="spring" id="spring" value="Watercress">Watercress
+          <br>
+        </form>  
+    </div>
+  </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+</html>
